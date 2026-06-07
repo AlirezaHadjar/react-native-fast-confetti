9984ae40e6752efefe3b00ca8e5dca4f6e9d4ec0
Alireza Hadjar <asiahadjar@gmail.com>
2026-06-07 14:39:37 +0200

    chore: update deps to expo v56

--- kitchen-sink/babel.config.js
+++ kitchen-sink/babel.config.js
@@ -1,17 +1,7 @@
-const path = require('path');
-const { getConfig } = require('react-native-builder-bob/babel-config');
-const pkg = require('../package.json');
-
-// eslint-disable-next-line no-undef
-const root = path.resolve(__dirname, '..');
-
 module.exports = function (api) {
   api.cache(true);
 
-  return getConfig(
-    {
-      presets: ['babel-preset-expo'],
-    },
-    { root, pkg }
-  );
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
